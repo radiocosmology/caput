f23865ea2a51d508aa1f1ce175dfadd4ca76fbfa
Kiyoshi Masui <kiyo@physics.ubc.ca>
2013-10-03 12:22:39 -0700

    Wrote python object hasher.

--- caput/cache_tools.py
+++ caput/cache_tools.py
@@ -1,14 +1,15 @@
 """Tools for caching the results of a long computation to disk.
 
 All data products are assumed to be functions of three things: input_data,
-parameters, and optionally the code that generates them (as specifed by a git
-sha1, or installed version).
+parameters, and optionally the code that generates them (as specified by a git
+SHA1, or installed version).
 
 """
 
-
 import subprocess
+import hashlib
 from os import path
+
 
 
 def get_git_dir(module_):
@@ -23,7 +24,7 @@ def get_git_dir(module_):
     try:
         file_path = module_.__file__
     except AttributeError:
-        msg = "Argumment must be a module or package."
+        msg = "Argument must be a module or package."
         raise TypeError(msg)
     file_path = file_path.split('/')
     # Search the path tree for the .git directory, starting at the deepest
@@ -37,13 +38,13 @@ def get_git_dir(module_):
         raise UnversionedError(msg)
 
 
-def get_package_sha1(module_):
+def get_package_commit(module_):
     """For a module versioned under git, get the SHA1 of the current commit.
 
     """
 
     git_dir = get_git_dir(module_)
-    # Run git show to get teh current commit of caput.
+    # Run `git show` to get the current commit of caput.
     call = ['git', '--git-dir=' + git_dir, 'show'] 
     proc = subprocess.Popen(call, stdout=subprocess.PIPE)
     ret = proc.wait()
@@ -54,6 +55,78 @@ def get_package_sha1(module_):
     commit = commit_line.split()[1]
     return commit
     
+
+def hash_obj(obj):
+    """Reproducibly compute a hash of any object easily represented in YAML.
+    
+    Supported types are `dict`, `list`, `set`, `None`, `str`, `int`, `float`,
+    `complex`,
+    `bool`, and any nesting (but not recursion) there of. Any other types will
+    interpreted as one of these types on a best effort basis.  Note in
+    particular that tuples and lists hash the same if their contents are the
+    same.  This is a consequence of duck typing.
+
+    Hash should be stable across versions of python and platforms (unlike the
+    built-in `hash()`). In general, if two values compare as being equal, they
+    should hash the same.  That is `1`, `1.00`, and `1.0 + 0j` and `True` all
+    have the same hash. It is not easy to engineer a hash collision. `a` hashes
+    differently to `repr(a)` and most other representations you might try 
+    (unless `a` is a string).
+
+    """
+    
+    string_to_hash = ''
+    # Cases for various types.
+    if isinstance(obj, basestring):
+        # Any string.
+        # This case must come before the sequence case since strings are
+        # iterable.
+        string_to_hash += obj
+    elif hasattr(obj, 'keys') and hasattr(obj, 'items'):
+        # Map/dict.
+        # This case must come before the sequence case since dicts are
+        # iterable.
+        string_to_hash += hash_obj('map')
+        # Hash the keys, and values then sort by key hashes.
+        hashed_pairs = [(hash_obj(k), hash_obj(v)) for k, v in obj.items()]
+        hashed_pairs.sort()
+        for h_key, h_value in hashed_pairs:
+            string_to_hash += h_key + h_value
+    elif hasattr(obj, '__iter__'):
+        # Sequence/list or Unordered set.
+        entry_hashes = []
+        for entry in obj:
+            entry_hashes.append(hash_obj(entry))
+        try: 
+            tmp = obj[0]
+        except IndexError:
+            # Sequence/list of length 0.
+            string_to_hash += hash_obj('seq')
+        except TypeError:
+            # Unordered/set.
+            string_to_hash += hash_obj('set')
+            entry_hashes.sort()
+        else:
+            # Sequence/list.
+            string_to_hash += hash_obj('seq')
+        string_to_hash += ''.join(entry_hashes)
+    elif obj is None:
+        string_to_hash += hash_obj('null')
+    else:
+        # Try to interpret as a number.
+        try:
+            string_to_hash += hash_obj('num')
+            if obj == obj.real:
+                obj = obj.real
+                if int(obj) == obj:
+                    obj = int(obj)
+            string_to_hash += repr((obj))
+        except TypeError, ValueError:
+            msg = "Argument type could not be interpreted."
+            raise TypeError(msg)
+    # Compute the hash.
+    hash_ = hashlib.sha1(string_to_hash).hexdigest()
+    return hash_
 
 
 class UnversionedError(Exception):
--- tests/test_cache_tools.py
+++ tests/test_cache_tools.py
@@ -39,8 +39,45 @@ class TestGitSHA1(unittest.TestCase):
         # Parse stout.
         commit_line = proc.stdout.readline()
         commit = commit_line.split()[1]
-        self.assertEqual(cache_tools.get_package_sha1(caput), commit)
-        self.assertEqual(cache_tools.get_package_sha1(cache_tools), commit)
+        self.assertEqual(cache_tools.get_package_commit(caput), commit)
+        self.assertEqual(cache_tools.get_package_commit(cache_tools), commit)
+
+class TestHashParams(unittest.TestCase):
+
+    def test_hash_equal(self):
+        """Make sure two equal dictionaries hash the same."""
+        
+        # Two dictionaries that are the same but defined differently.
+        a = {'a' : 5, 'b' : (10, 6, 8.000), 'aa' : {'f' : 'fff', 'g': 'gg'},
+             5 : {'s', 1, 3}, 10 : 3j}
+        b = {'aa' : {'g': 'gg', 'f' : 'fff'}, 'a' : 5, 'b' : [10, 6 + 0j, 8.],
+             5 : {3, 's', 1}, 10 : 0 + 3.0j}
+        self.assertEqual(cache_tools.hash_obj(a), cache_tools.hash_obj(b))
+        # Break Equality.
+        b['b'][2] = 8.001
+        self.assertNotEqual(cache_tools.hash_obj(a), cache_tools.hash_obj(b))
+        # Regression test.
+        self.assertEqual(cache_tools.hash_obj(a), 
+                         '0dc008e6282cc273ca6d5bfb7b8d1c6998623fbb')
+
+    def test_equivalent(self):
+        """Manually construct a few hashes."""
+
+        h = cache_tools.hash_obj
+        self.assertEqual(h(5.000 + 0.00j), h(h('num') + '5'))
+        self.assertEqual(h(None), h(h('null')))
+        self.assertEqual(h([7, 'a', 1]), h(h('seq') + h(7) + h('a') + h(1)))
+        self.assertEqual(h({7, 'a', 1, 42}),
+                 h(h('set') + ''.join(sorted([h(7), h(42), h('a'), h(1)]))))
+        
+        d = {4 : 5, 'a' : 'b', 'c' : 42}
+        dhash = [[h(4), h(5)], [h('a'), h('b')], [h('c'), h(42)]]
+        dhash = sorted(dhash)
+        dhash = [ x[0] + x[1] for x in dhash ]
+        dhash = h('map') + ''.join(dhash)
+        self.assertEqual(h(d), h(dhash))
+        
+
 
         
 
