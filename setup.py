d0322bbabb81c084274b3b9e94acbd24cf943fe2
Kiyoshi Masui <kiyo@physics.ubc.ca>
2015-12-22 10:18:22 -0800

    Fix RTD setup.

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
+import os
 
+
+REQUIRES = ['numpy', 'h5py', 'mpi4py', 'PyYAML']
+
+# Don't install requirements if on ReadTheDocs build system.
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    requires = []
+else:
+    requires = REQUIRES
 
 setup(
     name = 'caput',
     version = 0.1,
     packages = ['caput', 'caput.tests'],
     scripts=['scripts/caput-pipeline'],
-    install_requires = ['numpy', 'h5py', 'mpi4py', 'PyYAML'],
+    install_requires=requires,
 
     # metadata for upload to PyPI
     author = "Kiyo Masui, J. Richard Shaw",
