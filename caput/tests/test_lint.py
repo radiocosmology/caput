f8dbaab4c5d30856e6de9077f8db19f01c033220
Rick Nitsche <rick@phas.ubc.ca>
2020-12-11 09:48:39 -0800

    test(lint): add tests for the script's new lint command

---
+++ caput/tests/test_lint.py
@@ -0,0 +1,98 @@
+# === Start Python 2/3 compatibility
+from __future__ import absolute_import, division, print_function, unicode_literals
+from future.builtins import *  # noqa  pylint: disable=W0401, W0614
+from future.builtins.disabled import *  # noqa  pylint: disable=W0401, W0614
+
+# === End Python 2/3 compatibility
+
+import pytest
+import tempfile
+import yaml
+
+from click.testing import CliRunner
+
+from caput.config import Property
+from caput.pipeline import TaskBase
+from caput.scripts import runner as caput_script
+
+
+class DoNothing(TaskBase):
+    pass
+
+
+class DoNothing2(DoNothing):
+    a_list = Property(proptype=list)
+
+
+@pytest.fixture()
+def simple_config():
+    yield {
+        "pipeline": {
+            "tasks": [
+                {
+                    "type": "caput.tests.test_lint.DoNothing",
+                    "out": "out1",
+                },
+                {
+                    "type": "caput.tests.test_lint.DoNothing",
+                    "out": "out2",
+                    "in": "out1",
+                },
+                {
+                    "type": "caput.tests.test_lint.DoNothing2",
+                    "in": "out2",
+                    "out": "out3",
+                    "requires": "out1",
+                    "params": {
+                        "a_list": [1],
+                    },
+                },
+            ]
+        }
+    }
+
+
+def write_to_file(config_json):
+    temp = tempfile.NamedTemporaryFile(mode="w+t", delete=False)
+    temp.write(yaml.dump(config_json))
+    temp.flush()
+    return temp.name
+
+
+def test_load_yaml(simple_config):
+    test_runner = CliRunner()
+    config_file = write_to_file(simple_config)
+    result = test_runner.invoke(caput_script.lint_config, [config_file])
+    assert result.exit_code == 0
+
+
+def test_unknown_task(simple_config):
+    test_runner = CliRunner()
+    simple_config["pipeline"]["tasks"][0]["type"] = "what.was.the.name.of.my.Task"
+    config_file = write_to_file(simple_config)
+    result = test_runner.invoke(caput_script.lint_config, [config_file])
+    assert result.exit_code != 0
+
+
+def test_wrong_type(simple_config):
+    test_runner = CliRunner()
+    simple_config["pipeline"]["tasks"][2]["params"]["a_list"] = 1
+    config_file = write_to_file(simple_config)
+    result = test_runner.invoke(caput_script.lint_config, [config_file])
+    assert result.exit_code != 0
+
+
+def test_lonely_in(simple_config):
+    test_runner = CliRunner()
+    simple_config["pipeline"]["tasks"][2]["in"] = "foo"
+    config_file = write_to_file(simple_config)
+    result = test_runner.invoke(caput_script.lint_config, [config_file])
+    assert result.exit_code != 0
+
+
+def test_lonely_requires(simple_config):
+    test_runner = CliRunner()
+    simple_config["pipeline"]["tasks"][2]["requires"] = "bar"
+    config_file = write_to_file(simple_config)
+    result = test_runner.invoke(caput_script.lint_config, [config_file])
+    assert result.exit_code != 0
