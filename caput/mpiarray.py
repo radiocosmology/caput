f9bfdc497a72d7a9f1f504381cedc5635d218d8c
anjakefala <anja.kefala@gmail.com>
2021-02-16 18:59:20 -0800

    fix: do not raise exception for ndarray.view() case that occurs within wrap()

--- caput/mpiarray.py
+++ caput/mpiarray.py
@@ -1233,7 +1233,9 @@ class MPIArray(np.ndarray):
         if hasattr(obj, "axis"):
             axis = obj.axis
         else:
-            raise Exception("Cannot construct output axis")
+            # in the middle of an np.ndarray.view()
+            # user will have to set the attributes themselves
+            return
 
         # Get shape and offset
         lshape = self.shape
