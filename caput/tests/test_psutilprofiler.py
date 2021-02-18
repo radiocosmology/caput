18eac0438aa5dffe2cb8325e83421b1a3ef30641
Rick Nitsche <rick@phas.ubc.ca>
2021-02-17 17:19:30 -0800

    tests(profiling): add psutil profiler test

---
+++ caput/tests/test_psutilprofiler.py
@@ -0,0 +1,10 @@
+"""Test running the caput.pipeline."""
+
+from caput.tests import conftest
+
+
+def test_pipeline():
+    """Test profiling a very simple pipeline."""
+    result = conftest.run_pipeline(["--profile", "--profiler=psutil"])
+    print(result.output)
+    assert result.exit_code == 0
