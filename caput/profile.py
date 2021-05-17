a626984c846ce67b706ab5b37371aeae3f4dfa4c
Richard Shaw <richard@phas.ubc.ca>
2021-05-17 15:09:18 -0700

    feat(IOUsage): a context maanger for tracking IO done within a block

--- caput/profile.py
+++ caput/profile.py
@@ -87,3 +87,79 @@ class Profiler:
             self._pr.stop()
             with open(f"profile_{rank:0{rank_length}}.txt", "w") as fh:
                 fh.write(self._pr.output_text(unicode=True))
+
+
+class IOUsage:
+    """A context manager that gives the amount of IO done.
+
+    To access the IO usage the context manager object must be created and bound to a
+    variable *before* the *with* statement.
+
+    >>> u = IOUsage()
+    >>> with u:
+    ...     print("do some IO in here")
+    do some IO in here
+    >>> print(u.usage)  #doctest: +ELLIPSIS
+    {...}
+
+    Parameters
+    ----------
+    logger
+        If a logging object is passed the values of the IO done counters are logged
+        at INFO level.
+    """
+
+    _start = None
+
+    def __init__(self, logger: Optional[logging.Logger] = None):
+        self._logger = logger
+        self._usage = {}
+
+    @staticmethod
+    def _get_io():  # pylint: disable=no-self-use
+        # Get the cumulative IO performed
+
+        import psutil
+
+        if psutil.MACOS:
+            d = psutil.disk_io_counters()
+        else:
+            p = psutil.Process()
+            d = p.io_counters()
+
+        # There doesn't seem to be a public API for this
+        return d._asdict()
+
+    @staticmethod
+    def _units(key):  # pylint: disable=no-self-use
+        # Try and infer the units for this particular counter
+
+        suffix = key.split("_")[-1]
+
+        if suffix == "count":
+            return ""
+        elif suffix == "time":
+            return "ms"
+        elif suffix == "bytes":
+            return "bytes"
+        else:
+            return ""
+
+    def __enter__(self):
+        self._start = self._get_io()
+
+    def __exit__(self, *args, **kwargs):
+
+        f = self._get_io()
+
+        for name in f:
+            self._usage[name] = f[name] - self._start[name]
+
+        if self._logger:
+            for key, value in self.usage.items():
+                self._logger.info(f"IO usage({key}): {value} {self._units(key)}")
+
+    @property
+    def usage(self):
+        """The IO usage within the block."""
+        return self._usage.copy()
