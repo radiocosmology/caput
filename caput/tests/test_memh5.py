c905a07baa47ef4f5d19536a7b5c26ecb07a7736
leungcalvin <calvinl@mit.edu>
2021-10-28 07:06:55 -0700

    style(black): reformat test_copy()

--- caput/tests/test_memh5.py
+++ caput/tests/test_memh5.py
@@ -80,27 +80,31 @@ class TestGroup(unittest.TestCase):
     def test_copy(self):
         # create a rudimentary data tree
         a = memh5.MemDiskGroup()
-        a.create_dataset('first',data=np.arange(10))
-        b = a.create_group('second')
-        b.attrs['an_attribute'] = (1,2,3)
-        b.create_dataset('third',data=np.zeros(10))
-        b['third'].attrs['fill_value'] = 0 + 0j
+        a.create_dataset("first", data=np.arange(10))
+        b = a.create_group("second")
+        b.attrs["an_attribute"] = (1, 2, 3)
+        b.create_dataset("third", data=np.zeros(10))
+        b["third"].attrs["fill_value"] = 0 + 0j
 
         # copy it
 
-
         acopy = a.copy()
-        assert (a['first'][:] == acopy['first'][:]).all()
-        assert (a['second']['third'][:] == acopy['second/third'][:]).all()
-        assert (a['second'].attrs['an_attribute'] == acopy['second'].attrs['an_attribute'])
+        assert (a["first"][:] == acopy["first"][:]).all()
+        assert (a["second"]["third"][:] == acopy["second/third"][:]).all()
+        assert (
+            a["second"].attrs["an_attribute"] == acopy["second"].attrs["an_attribute"]
+        )
 
         # try changing data
-        a['first'][:] += 1
-        assert not (a['first'][:] == acopy['first'][:]).all()
+        a["first"][:] += 1
+        assert not (a["first"][:] == acopy["first"][:]).all()
 
         # try changing attrs
-        a['second'].attrs['an_attribute'] = (2,3,4)
-        assert not (a['second'].attrs['an_attribute'] == acopy['second'].attrs['an_attribute'])
+        a["second"].attrs["an_attribute"] = (2, 3, 4)
+        assert not (
+            a["second"].attrs["an_attribute"] == acopy["second"].attrs["an_attribute"]
+        )
+
 
 class TestH5Files(unittest.TestCase):
     """Tests that make hdf5 objects, convert to mem and back."""
