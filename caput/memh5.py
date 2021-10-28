e392dc9ea8745afeb57c149cc3ece964aa4096d9
leungcalvin <calvinl@mit.edu>
2021-10-28 06:58:08 -0700

    refactor(copy): move copy from BasicCont to MemDiskGroup where it belongs

--- caput/memh5.py
+++ caput/memh5.py
@@ -1774,6 +1774,41 @@ class MemDiskGroup(_BaseGroup):
                 **kwargs
             )
 
+    def copy(self):
+        """
+        Traverses a MemDiskGroup tree and deepcopies its datasets.
+        If the dataset is not a np.array this will complain and fail.
+        """
+        # the recursive function that does the moving of stuff from f to g
+        def _copy(f, g):
+            copyattrs(f.attrs, g.attrs, convert_strings=True)
+            for key in f.keys():
+                if is_group(f[key]):
+                    g_key = g.create_group(key)
+                    _copy(f[key], g_key)
+                else:
+                    try:
+                        g.create_dataset(
+                            key,
+                            shape=f[key].shape,
+                            dtype=f[key].dtype,
+                            data=np.ndarray.copy(
+                                f[key][:], order="A"
+                            ),  # ensure that deep copies are actually made; this needs to be generalized for HDF5 files.
+                        )
+                    except AttributeError:
+                        g.create_dataset(key, data=f[key])
+                    copyattrs(f[key].attrs, g[key].attrs)
+                    if hasattr(f, "index_map") and hasattr(g, "create_index_map"):
+                        for key in f.index_map.keys():
+                            g.create_index_map(key, f.index_map[key])
+
+        # make new object like the old one
+        new = self.__class__(distributed=self.distributed, comm=self.comm)
+        # move stuff from self to new
+        _copy(self, new)
+        return new
+
 
 class BasicCont(MemDiskGroup):
     """Basic high level data container.
@@ -1804,44 +1839,6 @@ class BasicCont(MemDiskGroup):
 
             if "order" not in self._data["history"].attrs:
                 self._data["history"].attrs["order"] = "[]"
-
-    def copy(self):
-        """
-        Makes a copy of a BasicCont which deepcopies its datasets.
-        Could be generalized to MemGroups whose datasets are not all numpy arrays.
-        In particular, if the data structure is hierarchical (datasets more than one layer deep), this will complain and fail.
-        """
-        # the recursive function that does the moving of stuff from f to g
-        def _copy(
-            f, g
-        ):
-            copyattrs(f.attrs, g.attrs, convert_strings=True)
-            for key in f.keys():
-                if is_group(f[key]):
-                    g_key = g.create_group(key)
-                    _copy(f[key], g_key)
-                else:
-                    try:
-                        g.create_dataset(
-                            key,
-                            shape=f[key].shape,
-                            dtype=f[key].dtype,
-                            data=np.ndarray.copy(
-                                f[key][:], order="A"
-                            ),  # ensure that deep copies are actually made.
-                        )
-                    except AttributeError:
-                        g.create_dataset(key, data=f[key])
-                    copyattrs(f[key].attrs, g[key].attrs)
-                    if hasattr(f, "index_map") and hasattr(g, "create_index_map"):
-                        for key in f.index_map.keys():
-                            g.create_index_map(key, f.index_map[key])
-
-        # make new object like the old one
-        new = BasicCont(distributed=self.distributed, comm=self.comm)
-        # move stuff from f to new
-        _copy(self, new)
-        return new
 
     @property
     def history(self):
