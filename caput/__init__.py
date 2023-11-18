9b76864e037a9acb6d2b7511cf91881ee415e3b4
Richard Shaw <richard@phas.ubc.ca>
2023-11-17 16:03:35 -0800

    fix: upgrade versioneer and change to non-vendored mode for Python 3.12

--- caput/__init__.py
+++ caput/__init__.py
@@ -17,7 +17,6 @@ Submodules
     tod
     weighted_median
 """
-from ._version import get_versions
+from . import _version
 
-__version__ = get_versions()["version"]
-del get_versions
+__version__ = _version.get_versions()["version"]
