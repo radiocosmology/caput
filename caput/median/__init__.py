e5459d297a4c18eedbf9a70f810d6e24259f8f8e
Liam Gray <lgray@phas.ubc.ca>
2025-06-17 16:42:32 -0700

    feat(median): move weighted median code to a submodule

---
+++ caput/median/__init__.py
@@ -0,0 +1,10 @@
+"""caput.median.
+
+Fast weighted median and moving weighted median.
+"""
+
+from .weighted import (
+    moving_weighted_median as moving_weighted_median,
+    quantile as quantile,
+    weighted_median as weighted_median,
+)
