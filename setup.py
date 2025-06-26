65004b3e7280681a4f8020a2e09b66409021dcf1
Liam Gray <lgray@phas.ubc.ca>
2025-06-26 15:04:24 -0700

    feat(astro): make a new 'astro' module, including time, units, coordinates

--- setup.py
+++ setup.py
@@ -43,8 +43,8 @@ extensions = [
         extra_link_args=omp_args,
     ),
     Extension(
-        name="caput.coordinates.coord",
-        sources=["caput/coordinates/coord.pyx"],
+        name="caput.astro.coordinates.coord",
+        sources=["caput/astro/coordinates/coord.pyx"],
         include_dirs=[numpy.get_include()],
         extra_compile_args=[*omp_args, "-g0", "-O3"],
         extra_link_args=omp_args,
