ddf50df2984984a18ac8348a6224060fad363256
Liam Gray <lgray@phas.ubc.ca>
2025-08-01 17:41:12 -0700

    feat(algorithms): make a submodule for fast algorithm implementations

---
+++ caput/algorithms/__init__.py
@@ -0,0 +1,18 @@
+"""caput.algorithms.
+
+Fast implementations of various common algorithms.
+
+Submodules
+----------
+.. autosummary::
+   :toctree: _autosummary
+
+   random
+"""
+
+from . import (
+    fft as fft,
+    invert_no_zero as invert_no_zero,
+    median as median,
+    random as random,
+)
