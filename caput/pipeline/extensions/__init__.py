de31e2a1678bfaf84744d15c99b9a8f70706c43e
Liam Gray <lgray@phas.ubc.ca>
2025-08-18 14:42:38 -0700

    fix(pipeline): fix some imports

--- caput/pipeline/extensions/__init__.py
+++ caput/pipeline/extensions/__init__.py
@@ -1,8 +1,9 @@
 """Extensions for `TaskBase` and helper code to run pure python pipelines."""
 
-from . import _extensions, _pytools
+from . import _configtypes, _extensions, _pytools
+from ._configtypes import *
 from ._extensions import *
 from ._pytools import *
 
 
-__all__ = [*_extensions.__all__, *_pytools.__all__]
+__all__ = [*_configtypes.__all__, *_extensions.__all__, *_pytools.__all__]
